971fb50f7080274ea5fefe0fbb5862c4585cfc30
高見知英 <chie@onpu-tamago.net>
2016-03-18 15:02:27 +0900

    Pico1.0 correspondence

--- pico_breadcrumbs.php
+++ pico_breadcrumbs.php
@@ -6,88 +6,44 @@
  * @author nebman
  */
  
-class Pico_Breadcrumbs {
+class Pico_Breadcrumbs extends AbstractPicoPlugin {
+
+  protected $enabled = false;
 
 	private $breadcrumbs = array();
 	private $page_names = array();
 	private $settings = array();
 
-	public function plugins_loaded()
-	{
-		
+  public function onConfigLoaded(array &$config)
+  {
+		$this->settings = $config;
 	}
 
-	public function config_loaded(&$settings)
-	{
-		$this->settings = $settings;
-	}
-	
-	public function request_url(&$url)
-	{
+  public function onRequestUrl(&$url)
+  {
 		$this->breadcrumbs = explode('/', $url);		
 	}
 	
-	public function before_load_content(&$file)
-	{
-		
-	}
-	
-	public function after_load_content(&$file, &$content)
-	{
-		
-	}
-	
-	public function before_404_load_content(&$file)
-	{
-		
-	}
-	
-	public function after_404_load_content(&$file, &$content)
-	{
-		
-	}
-	
-	public function before_read_file_meta(&$headers)
-	{
+  public function onMetaHeaders(array &$headers)
+  {
 		$headers['author_url'] = 'Author_URL';
 	}
-	
-	public function file_meta(&$meta)
-	{
 
-	}
-
-	public function before_parse_content(&$content)
-	{
-		
-	}
-	
-	public function after_parse_content(&$content)
-	{
-		
-	}
-	
-	public function get_page_data(&$data, $page_meta)
-	{
-		
-	}
-	
-	public function get_pages(&$pages, &$current_page, &$prev_page, &$next_page)
-	{
+  public function onPagesLoaded(
+      array &$pages,
+      array &$currentPage = null,
+      array &$previousPage = null,
+      array &$nextPage = null
+  ) {
 		foreach( $pages as $page ){
 			$this->page_names[$page['url']] = $page['title'];
 		}
 	}
-	
-	public function before_twig_register()
-	{
-		
-	}
-	
-	public function before_render(&$twig_vars, &$twig, &$template)
-	{
+
+  public function onPageRendering(Twig_Environment &$twig, array &$twigVariables, &$templateName)
+  {
 		$breadcrumbs = array();
-		$url = $this->settings['base_url'];
+		$url = substr($this->settings['base_url'], 0, -1);
 		$baseurl = $url;
 		foreach ($this->breadcrumbs as $crumb) {
 			$url = $url . '/' . $crumb;
@@ -96,13 +52,9 @@ class Pico_Breadcrumbs {
 			$breadcrumbs[] = array('url' => $url, 'name' => $name, 'exists' => $exists );
 		}
 		
-		$twig_vars['breadcrumbs'] = $breadcrumbs;
+		$twigVariables['breadcrumbs'] = $breadcrumbs;
 	}
-	
-	public function after_render(&$output)
-	{
-		
-	}
+
   /*
    * URL to check whether the real URL ( copy from Pico source )
    *
@@ -112,11 +64,12 @@ class Pico_Breadcrumbs {
   private function url_exists($url)
   {
     $file = "";
+    $ext = $this->settings['content_ext'];
 		if($url) $file = $this->settings['content_dir'] . $url;
 		else $file = $this->settings['content_dir'] .'index';
 
-		if(is_dir($file)) $file = $this->settings['content_dir'] . $url .'/index'. CONTENT_EXT;
-		else $file .= CONTENT_EXT;
+		if(is_dir($file)) $file = $this->settings['content_dir'] . $url .'/index'. $ext;
+  		else $file .= $ext;
 		return file_exists($file);
   }
 	
